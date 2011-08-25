8e9ddb9baeb08f50533839b90c34a1153e0a3ade
Goran Schwarz <goran_schwarz@hotmail.com>
2011-08-25 14:03:51 +0000

    Just changed Version to 2.1.0.8.dev so I know this is latest build with asemon -> AseTune for test build

--- src/com/asetune/Version.java
+++ src/com/asetune/Version.java
@@ -11,8 +11,8 @@ public class Version
 {
 	public static final String PRODUCT_STRING     = "AseTune"; // Do not have spaces etc in this one
 //	public static final String VERSION_STRING     = "2.1.0";
-	public static final String VERSION_STRING     = "2.1.0.7.dev";
-	public static final String BUILD_STRING       = "2011-08-24/build 82";
+	public static final String VERSION_STRING     = "2.1.0.8.dev";
+	public static final String BUILD_STRING       = "2011-08-25/build 83";
 
 	public static final boolean IS_DEVELOPMENT_VERSION   = true; // if true: date expiration will be checked on startup
 	public static final String  DEV_VERSION_EXPIRE_STR  = "2011-11-30";  // "YYYY-MM-DD"
