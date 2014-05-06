7a58d7ae04218a9768163949c109dc8fa2585661
Goran Schwarz <goran_schwarz@hotmail.com>
2014-05-06 17:57:53 +0000

    dummy commit

--- src/com/asetune/Version.java
+++ src/com/asetune/Version.java
@@ -12,7 +12,7 @@ public class Version
 {
 	public static       String PRODUCT_STRING     = "AseTune"; // Do not have spaces etc in this one
 //	public static final String VERSION_STRING     = "3.3.0";
-	public static final String VERSION_STRING     = "3.3.0.11.dev";
+	public static final String VERSION_STRING     = "3.3.0.11.dev"; 
 	public static final String BUILD_STRING       = "2014-05-06/build 152"; 
 
 	public static final boolean IS_DEVELOPMENT_VERSION  = true; // if true: date expiration will be checked on startup
