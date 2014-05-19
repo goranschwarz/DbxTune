715b0e896ccbfefa987f52e1ffd9ae164abb9833
Goran Schwarz <goran_schwarz@hotmail.com>
2014-05-19 18:38:02 +0000

    * version string * also changed 2 info messages to debug

--- src/com/asetune/Version.java
+++ src/com/asetune/Version.java
@@ -12,8 +12,8 @@ public class Version
 {
 	public static       String PRODUCT_STRING     = "AseTune"; // Do not have spaces etc in this one
 //	public static final String VERSION_STRING     = "3.3.0";
-	public static final String VERSION_STRING     = "3.3.0.14.dev"; 
-	public static final String BUILD_STRING       = "2014-05-15/build 154"; 
+	public static final String VERSION_STRING     = "3.3.0.15.dev"; 
+	public static final String BUILD_STRING       = "2014-05-19/build 154"; 
 
 	public static final boolean IS_DEVELOPMENT_VERSION  = true; // if true: date expiration will be checked on startup
 	public static final String  DEV_VERSION_EXPIRE_STR  = "2014-10-30";  // "YYYY-MM-DD" 
