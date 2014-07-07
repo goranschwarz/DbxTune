0757d7acb42e0abd27aa71728f6f55b9fcad19ea
Goran Schwarz <goran_schwarz@hotmail.com>
2014-07-07 11:48:03 +0000

    Version string and history update

--- src/com/asetune/Version.java
+++ src/com/asetune/Version.java
@@ -11,12 +11,12 @@ import java.util.Date;
 public class Version
 {
 	public static       String PRODUCT_STRING     = "AseTune"; // Do not have spaces etc in this one
-//	public static final String VERSION_STRING     = "3.3.0";
-	public static final String VERSION_STRING     = "3.3.0.20.dev"; 
-	public static final String BUILD_STRING       = "2014-06-27/build 159"; 
+	public static final String VERSION_STRING     = "3.4.0";
+//	public static final String VERSION_STRING     = "3.4.0.0.dev"; 
+	public static final String BUILD_STRING       = "2014-07-207/build 160"; 
 
 	public static final boolean IS_DEVELOPMENT_VERSION  = true; // if true: date expiration will be checked on startup
-	public static final String  DEV_VERSION_EXPIRE_STR  = "2014-10-30";  // "YYYY-MM-DD" 
+	public static final String  DEV_VERSION_EXPIRE_STR  = "2015-10-30";  // "YYYY-MM-DD" 
 	public static       Date    DEV_VERSION_EXPIRE_DATE = null;
 
 	public static final String SOURCE_DATE_STRING = "$Date$";
