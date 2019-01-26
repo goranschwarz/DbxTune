2413135db35ef47accb2f9612061c410b7f34ae9
Goran Schwarz <goran_schwarz@hotmail.com>
2019-01-26 22:20:26 +0000

    Added copyright text to all java files

--- src/com/asetune/utils/ArrayUtils.java
+++ src/com/asetune/utils/ArrayUtils.java
@@ -1,3 +1,23 @@
+/*******************************************************************************
+ * Copyright (C) 2010-2019 Goran Schwarz
+ * 
+ * This file is part of DbxTune
+ * DbxTune is a family of sub-products *Tune, hence the Dbx
+ * Here are some of the tools: AseTune, IqTune, RsTune, RaxTune, HanaTune, 
+ *          SqlServerTune, PostgresTune, MySqlTune, MariaDbTune, Db2Tune, ...
+ * 
+ * DbxTune is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * DbxTune is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with DbxTune.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package com.asetune.utils;
 
 public class ArrayUtils
