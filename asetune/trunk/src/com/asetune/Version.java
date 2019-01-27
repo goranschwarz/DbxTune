ac2b8bdd6461d9aca95b3bcc85b5fb46a155f9d0
gorans <gorans@e740d447-5320-49a2-a665-7d230cca4707>
2019-01-27 18:50:57 +0000

    Updated version string

--- asetune/trunk/src/com/asetune/Version.java
+++ asetune/trunk/src/com/asetune/Version.java
@@ -18,28 +18,6 @@
  * You should have received a copy of the GNU General Public License
  * along with DbxTune.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
-/* ----------------------------------------------------------------------------
-**  Copyright (C) 2010-2018 Göran Schwarz
-**
-**  This file is part of DbxTune
-**  DbxTune is a family of sub-products *Tune, hence the Dbx
-**  Here are some of the tools: AseTune, IqTune, RsTune, RaxTune, HanaTune, 
-**           SqlServerTune, PostgresTune, MySqlTune, MariaDbTune, Db2Tune, ...
-**
-**  DbxTune is free software: you can redistribute it and/or modify
-**  it under the terms of the GNU General Public License as published by
-**  the Free Software Foundation, either version 3 of the License, or
-**  (at your option) any later version.
-**
-**  DbxTune is distributed in the hope that it will be useful,
-**  but WITHOUT ANY WARRANTY; without even the implied warranty of
-**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**  GNU General Public License for more details.
-**
-**  You should have received a copy of the GNU General Public License
-**  along with DbxTune.  If not, see <http://www.gnu.org/licenses/>.
-** ----------------------------------------------------------------------------
-*/
 /**
  * @author <a href="mailto:goran_schwarz@hotmail.com">Goran Schwarz</a>
  */
@@ -53,8 +31,8 @@ public class Version
 {
 	public static       String PRODUCT_STRING     = "AseTune";      // Do not have spaces etc in this one
 //	public static final String VERSION_STRING     = "4.0.0";        // Use this for public releases
-	public static final String VERSION_STRING     = "4.0.0.11.dev"; // Use this for early releases
-	public static final String BUILD_STRING       = "2019-01-16/build 259";
+	public static final String VERSION_STRING     = "4.0.0.12.dev"; // Use this for early releases
+	public static final String BUILD_STRING       = "2019-01-27/build 260";
 
 	public static final boolean IS_DEVELOPMENT_VERSION  = true; // if true: date expiration will be checked on startup
 	public static final String  DEV_VERSION_EXPIRE_STR  = "2020-11-30";  // "YYYY-MM-DD" 
