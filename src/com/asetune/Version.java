b7b3746de64b59e90ae717297dd7a88aac88d7b3
Goran Schwarz <goran_schwarz@hotmail.com>
2019-04-26 21:37:24 +0200

    updated version string (which I always forgets)

--- src/com/asetune/Version.java
+++ src/com/asetune/Version.java
@@ -31,10 +31,10 @@ public class Version
 {
 	public static       String PRODUCT_STRING     = "AseTune";      // Do not have spaces etc in this one
 //	public static final String VERSION_STRING     = "4.0.0";        // Use this for public releases
-	public static final String VERSION_STRING     = "4.0.0.36.dev"; // Use this for early releases
-	public static final String BUILD_STRING       = "2019-04-25/build 279";
+	public static final String VERSION_STRING     = "4.0.0.37.dev"; // Use this for early releases
+	public static final String BUILD_STRING       = "2019-04-26/build 280";
 
-	public static final String GIT_DATE_STRING    = "2019-04-25"; // try to update this
+	public static final String GIT_DATE_STRING    = "2019-04-26"; // try to update this
 	public static final String GIT_REVISION_STR   = "500";        // used by CheckForUpdates --- update this on every check-in (emulates Subversion "Revision:" tag)
 
 	public static final boolean IS_DEVELOPMENT_VERSION  = true; // if true: date expiration will be checked on startup
