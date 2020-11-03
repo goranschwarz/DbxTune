e4d5c4e583a368fcee430890af1529bcba86d03c
Göran Schwarz <goran_schwarz@hotmail.com>
2020-11-03 01:42:25 +0100

    Removed one file, and updated Version

--- src/com/asetune/Version.java
+++ src/com/asetune/Version.java
@@ -31,11 +31,11 @@ public class Version
 {
 	public static       String PRODUCT_STRING     = "AseTune";      // Do not have spaces etc in this one
 //	public static final String VERSION_STRING     = "4.1.0";        // Use this for public releases
-	public static final String VERSION_STRING     = "4.1.0.7.dev"; // Use this for early releases
-	public static final String BUILD_STRING       = "2020-10-15/build 336";
+	public static final String VERSION_STRING     = "4.1.0.8.dev"; // Use this for early releases
+	public static final String BUILD_STRING       = "2020-11-03/build 337";
 
-	public static final String GIT_DATE_STRING    = "2020-10-15"; // try to update this
-	public static final String GIT_REVISION_STR   = "501";         // used by CheckForUpdates --- update this on every check-in (emulates Subversion "Revision:" tag)
+	public static final String GIT_DATE_STRING    = "2020-11-03"; // try to update this
+	public static final String GIT_REVISION_STR   = "502";         // used by CheckForUpdates --- update this on every check-in (emulates Subversion "Revision:" tag)
 
 	public static final boolean IS_DEVELOPMENT_VERSION  = true; // if true: date expiration will be checked on startup
 	public static final String  DEV_VERSION_EXPIRE_STR  = "2022-11-30";  // "YYYY-MM-DD" 
